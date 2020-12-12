7082af2f4d42567a54cfcc1ba6825894b56561fd
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2020-12-12 16:25:46 -0300

    Added service-workes from CRA_v4

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import thunkMiddleware from 'redux-thunk'
 import './index.css';
 import 'tachyons';
 import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
 import { searchRobots, requestRobots } from './reducers';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const logger = createLogger();
 const rootReducers = combineReducers({searchRobots, requestRobots})
@@ -23,6 +24,10 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
